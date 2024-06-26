52d32e80d47d7c5d22ec1fca2a359105cffe5f56
adsk0165 <a.charkaoui@student.avans.nl>
2024-06-26 18:56:43 +0200

    login still bugged

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,12 @@
-const database = require('../dao/inmem-db');
+const bcrypt = require('bcrypt');
+const db = require('../dao/mysql-db');
 const logger = require('../util/logger');
-
-const db = require('../dao/mysql-db')
 
 const userService = {
     create: (user, callback) => {
         logger.info('create user', user);
 
-        // Controleer of alle vereiste velden aanwezig zijn
+        // Validate required fields
         const requiredFields = ['firstName', 'lastName', 'street', 'city', 'emailAdress', 'password', 'phoneNumber'];
         for (const field of requiredFields) {
             if (!user[field]) {
@@ -17,6 +16,48 @@ const userService = {
             }
         }
 
+        // Hash the password before storing it
+        bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+            if (err) {
+                logger.error(err);
+                callback(err, null);
+                return;
+            }
+
+            user.password = hashedPassword;
+
+            db.getConnection((err, connection) => {
+                if (err) {
+                    logger.error(err);
+                    callback(err, null);
+                    return;
+                }
+
+                connection.query(
+                    'INSERT INTO `user` SET ?',
+                    user,
+                    (error, results, fields) => {
+                        connection.release();
+
+                        if (error) {
+                            logger.error(error);
+                            callback(error, null);
+                        } else {
+                            const createdUser = { id: results.insertId, ...user };
+                            callback(null, {
+                                message: `User created with ID ${results.insertId}`,
+                                data: createdUser
+                            });
+                        }
+                    }
+                );
+            });
+        });
+    },
+
+    getAll: (callback) => {
+        logger.info('getAll');
+
         db.getConnection((err, connection) => {
             if (err) {
                 logger.error(err);
@@ -25,8 +66,7 @@ const userService = {
             }
 
             connection.query(
-                'INSERT INTO `user` SET ?',
-                user,
+                'SELECT id, firstName, lastName FROM `user`',
                 (error, results, fields) => {
                     connection.release();
 
@@ -34,10 +74,10 @@ const userService = {
                         logger.error(error);
                         callback(error, null);
                     } else {
-                        const createdUser = { id: results.insertId, ...user };
+                        logger.debug(results);
                         callback(null, {
-                            message: `User created with ID ${results.insertId}`,
-                            data: createdUser
+                            message: `Found ${results.length} users.`,
+                            data: results
                         });
                     }
                 }
@@ -45,68 +85,22 @@ const userService = {
         });
     },
 
-    getAll: (callback) => {
-        logger.info('getAll');
-        // database.getAll((err, data) => {
-        //     if (err) {
-        //         callback(err, null);
-        //     } else {
-        //         callback(null, {
-        //             message: `Found ${data.length} users.`,
-        //             data: data
-        //         });
-        //     }
-        // });
-        db.getConnection(function (err, connection) {
-            if (err) {
-                logger.error(err)
-                callback(err, null)
-                return
-            }
-
-            connection.query(
-                'SELECT id, firstName, lastName FROM `user`',
-                function (error, results, fields) {
-                    connection.release()
-
-                    if (error) {
-                        logger.error(error)
-                        callback(error, null)
-                    } else {
-                        logger.debug(results)
-                        callback(null, {
-                            message: `Found ${results.length} users.`,
-                            data: results
-                        })
-                    }
-                }
-            )
-        })
-    },
-
     getById: (id, callback) => {
         logger.info(`getUserById: ${id}`);
-        // database.getById(id, (err, user) => {
-        //     if (err) {
-        //         callback(err, null);
-        //     } else {
-        //         callback(null, user);
-        //     }
-        // });
-    
+
         db.getConnection((err, connection) => {
             if (err) {
                 logger.error(err);
                 callback(err, null);
                 return;
             }
-    
+
             connection.query(
                 'SELECT * FROM `user` WHERE `id` = ?',
                 [id],
                 (error, results, fields) => {
                     connection.release();
-    
+
                     if (error) {
                         logger.error(error);
                         callback(error, null);
@@ -123,24 +117,24 @@ const userService = {
                 }
             );
         });
-    },    
+    },
 
     update: (userId, newData, callback) => {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (newData.emailAdress && !emailPattern.test(newData.emailAdress)) {
-            const error = new Error('Invalid email adress');
-            error.status = 400; // Verander status naar 400 voor ongeldige e-mail
+            const error = new Error('Invalid email address');
+            error.status = 400;
             return callback(error, null);
         }
-    
-        // Valideer het telefoonnummer
+
+        // Validate phone number
         const phonePattern = /^06-\d{8}$|^06 \d{8}$|^06\d{8}$/;
         if (newData.phoneNumber && !phonePattern.test(newData.phoneNumber)) {
             const error = new Error('Invalid phone number');
             error.status = 400;
             return callback(error, null);
         }
-    
+
         const requiredFields = ['emailAdress'];
         for (const field of requiredFields) {
             if (!newData[field]) {
@@ -149,28 +143,29 @@ const userService = {
                 return callback(error, null);
             }
         }
-    
+
         logger.info(`Updating user with ID ${userId}`);
+
         db.getConnection((err, connection) => {
             if (err) {
                 logger.error(err);
                 callback(err, null);
                 return;
             }
-    
+
             connection.query(
                 'UPDATE `user` SET ? WHERE `id` = ?',
                 [newData, userId],
                 (error, results, fields) => {
                     connection.release();
-    
+
                     if (error) {
                         logger.error(error);
                         callback(error, null);
                     } else {
                         if (results.affectedRows === 0) {
                             const notFoundError = new Error(`User with ID ${userId} not found`);
-                            notFoundError.status = 404; // Verander status naar 404 voor niet gevonden gebruiker
+                            notFoundError.status = 404;
                             callback(notFoundError, null);
                         } else {
                             const updatedUser = { id: userId, ...newData };
@@ -184,22 +179,23 @@ const userService = {
             );
         });
     },
-    
+
     delete: (userId, callback) => {
         logger.info(`Deleting user with ID ${userId}`);
+
         db.getConnection((err, connection) => {
             if (err) {
                 logger.error(err);
                 callback(err, null);
                 return;
             }
-    
+
             connection.query(
                 'DELETE FROM `user` WHERE `id` = ?',
                 [userId],
                 (error, results, fields) => {
                     connection.release();
-    
+
                     if (error) {
                         logger.error(error);
                         callback(error, null);
@@ -219,7 +215,7 @@ const userService = {
                 }
             );
         });
-    }    
+    }
 };
 
 module.exports = userService;
